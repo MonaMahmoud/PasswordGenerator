8388c18708df7360fede3c0ea635ecd6f2944d22
MonaMahmoud <monamohamedfahmy@gmail.com>
2021-12-14 18:22:21 +1100

    Added descriptive comments to the code

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ generateBtn.addEventListener("click", writePassword);
 
 
 function generatePassword(){
-  //strings containing different password choices
+  //strings containing different password characters used to generate the password
   var upperAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var lowerAlphabet = "abcdefghijklmnopqrstuvwxyz";
   var specialAlphabet =' !"#$%&,()*+-./:;<=>?@[]_^`{}|~\\\'';
@@ -24,7 +24,7 @@ function generatePassword(){
 
   //password length entered by user
   var passwordLength;
-  //indicates whether password length has met required conditions (number between 8 and 128)
+  //indicates whether password length has met required conditions (a number between 8 and 128)
   var conditions=false;
 
   do{
@@ -33,9 +33,10 @@ function generatePassword(){
     //check that user entered a number between the specified range
     if(parseInt(passwordLength)!=NaN && passwordLength>=8 &&passwordLength<=128){
       passwordLength = parseInt(passwordLength);
+      //no need to ask user again, break the while loop condition
       conditions=true;
     }
-  //loop again until user inputs a number within specified range  
+  //wrong type or numberic value not in the specified range, loop until user inputs a number within specified range  
   }while(!conditions);
 
   //ask user for password choices and store them in boolean variables
@@ -49,28 +50,32 @@ function generatePassword(){
   //length of each choice section
   var sectionLength;
   
-  
-
+  //user declined all options, ask to include at least one
   if(!upperCase&&!lowerCase&&!numeric&&!specialChars){
     alert("Can't generate password. Please choose at least one option.");
   }
+  //at least one option was chosen, continue to generate password according to user choice
   else{
 
+    //loop generating the password according to length entered by user, loop breaks when length of generated password is the same as length entered by user
     for(i=0;i<passwordLength;){
+      //if upper case characters are included add one upper case character and increment password length counter
       if(upperCase){
         pass = pass.concat(upperAlphabet.charAt(Math.floor(Math.random() * upperAlphabet.length)).toString());
         i++;
       }
+      //if lower case characters are included and password length is less than user's enterd value add one lower case character and increment password length counter
       if(lowerCase&&i<passwordLength){
         pass = pass.concat(lowerAlphabet.charAt(Math.floor(Math.random() * lowerAlphabet.length)).toString());
         i++;
       }
-  
+      //if numeric characters are included and password length is less than user's enterd value add one number and increment password length counter
       if(numeric&&i<passwordLength){
         pass = pass.concat(numericAlphabet.charAt(Math.floor(Math.random() * numericAlphabet.length)).toString());
         i++;
       }
-  
+      
+      //if special characters are included and password length is less than user's enterd value add one special character and increment password length counter
       if(specialChars&&i<passwordLength){
         pass = pass.concat(specialAlphabet.charAt(Math.floor(Math.random() * specialAlphabet.length)).toString());
         i++;
@@ -79,6 +84,6 @@ function generatePassword(){
     }
 
   }
-
+  //return the generated password
   return pass;
 }
